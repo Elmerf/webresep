9023c4e8e526c642fee2f3f00e6cf5f71343d05e
Elmer Fiqi <elmerfiqi@gmail.com>
2021-07-10 12:45:41 +0700

    Search Recipes

--- src/handlers/recipes/get-all-recipes.js
+++ src/handlers/recipes/get-all-recipes.js
@@ -2,7 +2,18 @@ const Recipe = require('../../models/Recipe');
 
 const getAllRecipes = async (request, h) => {
   try {
-    const result = await Recipe.find({}).sort({createdAt: -1});
+    const {q} = request.query;
+    let result;
+    if (q !== undefined) {
+      result = await Recipe.find(
+          {
+            namaresep:
+              {
+                $regex: '.*' + q + '.*',
+                $options: 'i',
+              },
+          }).sort({createdAt: -1});
+    } else result = await Recipe.find({}).sort({createdAt: -1});
     return result;
   } catch (err) {
     return h.response(err).code(500);
