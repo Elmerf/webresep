84b78be8adafc41497a123116b840397ed424e0f
Elmer Fiqi <elmerfiqi@gmail.com>
2021-07-10 14:34:01 +0700

    add default image

--- src/handlers/recipes/get-recipe-image.js
+++ src/handlers/recipes/get-recipe-image.js
@@ -1,10 +1,15 @@
 const Recipe = require('../../models/Recipe');
+const fs = require('fs');
 
 const getRecipeImage = async (request, h) => {
   try {
     const {id} = request.params;
     const result = await Recipe.findOne({'_id': id}, 'namaresep').exec();
-    return h.file(`./src/assets/${result.namaresep}.jpg`);
+    const path = `./src/assets/${result.namaresep}.jpg`;
+    if (fs.existsSync(path)) {
+      return h.file(path);
+    }
+    return h.file('./src/assets/logo.jpg');
   } catch (err) {
     return h.response(err).code(500);
   }
